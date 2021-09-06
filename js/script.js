8289b2d668bcff6ba0018c04a8096123fd077f02
pacific-o <a.soltani63@yahoo.com>
2021-09-06 16:42:03 +0430

    final optimization

--- js/script.js
+++ js/script.js
@@ -10,30 +10,113 @@ const resumeEl = document.getElementById("resume-menu");
 const resumeCardsContainer = document.getElementById('resume-cards-container');
 
 
+// function to change proficiencies in home page
+var counter = 1;
+
+var myVar = setInterval(showTitle, 4000);
+
+function showTitle() {
+	var numOfTitles = document.getElementsByClassName("slides");
+	for (var i = 0; i < numOfTitles.length; i++) {
+		numOfTitles[i].className = "slides";
+	}
+	document.getElementsByClassName("slides")[counter].className = "slides fade-in";
+	counter++;
+
+	if (counter >= numOfTitles.length) {
+		counter = 0;
+	};
+
+};
+
+
+// function te set attribute for elements
 function setStyleSheet(url) {
 	var stylesheet = document.getElementById("stylesheet");
 	stylesheet.setAttribute('href', url);
 }
 
-$('#light-mode').click(() =>
-	setStyleSheet('./style/light-style.css')
-)
 
-$('#dark-mode').click(() =>
-	setStyleSheet('./style/dark-style.css')
-)
+// function to show and hide resume items
+function toggleCard(e) {
+	document.getElementById(e.target.dataset.target).classList.toggle('active-card');
+	resumeCardsContainer.classList.toggle('active-container');
+	resumeEl.classList.toggle('visible');
+	$(".resume-btn").toggleClass('translate');
+}
 
-$('#green-mode').click(() =>
-	setStyleSheet('./style/green-style.css')
-)
 
-$('#blue-mode').click(() =>
-	setStyleSheet('./style/blue-style.css')
-)
+// function to change resume items
+function cardChanger(e) {
+	document.getElementsByClassName('active-card')[0].classList.remove('active-card');
+	document.getElementById(e.target.dataset.target).classList.add('active-card');
+}
 
-$('#yellow-mode').click(() =>
-	setStyleSheet('./style/yellow-style.css')
-)
+
+$(document).ready(function () {
+
+// function to colors section slide toggle
+	$(".color-customizer").hover(function () {
+		$(".color-options").slideToggle('fast');
+	})
+
+
+// function to show all blogs information
+	$(".more-info-btn").each(function () {
+		$(this).click(function () {
+			$('.blog-card').not($(this).parent().parent()).toggle("slow");
+			$(this).parent().parent().find(".blog-more-info").slideToggle('slow');
+			$(this).parent().toggleClass('rotate');
+		})
+	});
+
+
+// function to like and unlike blogs
+	$(".like-btn").each(function () {
+		$(this).click(function () {
+			$(this).toggleClass('liked')
+		})
+	})
+
+
+// function to change color mode
+	$('#light-mode').click(() =>
+		setStyleSheet('./style/light-style.css')
+	)
+
+	$('#dark-mode').click(() =>
+		setStyleSheet('./style/dark-style.css')
+	)
+
+	$('#green-mode').click(() =>
+		setStyleSheet('./style/green-style.css')
+	)
+
+	$('#blue-mode').click(() =>
+		setStyleSheet('./style/blue-style.css')
+	)
+
+
+// click handler function to show resume section
+	$('.resume-link').click((e) =>
+		toggleCard(e)
+	)
+
+
+// click handler function to hide resume section
+	$('.card-closer').click((e) => {
+		toggleCard(e);
+	})
+
+
+// click handler function to change resume items
+	$('.resume-item').click((e) =>
+		cardChanger(e)
+	)
+
+
+});
+
 
 
 infoBtn.addEventListener('click', () =>
@@ -59,76 +142,3 @@ InfoFadeInBtn.addEventListener('click', () =>
 closeBtn.addEventListener('click', () =>
 	infoContainer.classList.remove('fade-in')
 );
-
-
-$('.resume-link').click((e) =>
-	toggleCard(e)
-)
-
-$('.card-closer').click((e) => {
-	toggleCard(e);
-})
-
-
-function toggleCard(e) {
-	document.getElementById(e.target.dataset.target).classList.toggle('active-card');
-	resumeCardsContainer.classList.toggle('active-container');
-	resumeEl.classList.toggle('visible');
-	$(".resume-btn").toggleClass('translate');
-}
-
-
-$('.resume-item').click((e) =>
-	cardChanger(e)
-)
-
-
-function cardChanger(e) {
-	document.getElementsByClassName('active-card')[0].classList.remove('active-card');
-	document.getElementById(e.target.dataset.target).classList.add('active-card');
-}
-
-
-var counter = 1;
-
-var myVar = setInterval(showTitle, 4000);
-
-function showTitle() {
-	var numOfTitles = document.getElementsByClassName("slides");
-	for (var i = 0; i < numOfTitles.length; i++) {
-		numOfTitles[i].className = "slides";
-	}
-	document.getElementsByClassName("slides")[counter].className = "slides fade-in";
-	counter++;
-
-	if (counter >= numOfTitles.length) {
-		counter = 0;
-	};
-
-};
-
-
-$(document).ready(function () {
-
-
-	$(".color-customizer").hover(function () {
-		$(".color-options").slideToggle('fast');
-	})
-
-
-	$(".more-info-btn").each(function () {
-		$(this).click(function () {
-			$('.blog-card').not($(this).parent().parent()).toggle("slow");
-			$(this).parent().parent().find(".blog-more-info").slideToggle('slow');
-			$(this).parent().toggleClass('rotate');
-		})
-	});
-
-	$(".like-btn").each(function () {
-		$(this).click(function () {
-			$(this).toggleClass('liked')
-		})
-	})
-
-
-});
